cafa75d222260de9d375567386f9ecd88d869896
nina <ninagbs@abv.bg>
2024-08-28 16:31:06 +0300

    added pdf download

--- src/App.js
+++ src/App.js
@@ -17,20 +17,62 @@ import Education from './components/Education';
 
 import allData from './data';
 import Image from './components/Image';
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+
+
 
 function App() {
   
+  const generatePDF = () => {
+    const input = document.getElementById('page');
+   // Temporarily make all text invisible for html2canvas
+   html2canvas(input).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("p", "mm", "a4");
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+
+    // Add links manually to the PDF
+    const linkElements = document.querySelectorAll('a');
+
+    linkElements.forEach(linkElement => {
+        const url = linkElement.href;
+        const rect = linkElement.getBoundingClientRect();
+        const inputRect = input.getBoundingClientRect();
+
+        // Calculate position of the link
+        const x = (rect.left - inputRect.left) * (pdfWidth / input.offsetWidth);
+        const y = (rect.top - inputRect.top) * (pdfHeight / input.offsetHeight);
+        let text = linkElement.textContent;
+text='                               ';
+
+        pdf.textWithLink(text, x, y, { url: url });
+    });
+
+    pdf.save("download.pdf");
+
+});
+};
   const [language, setLanguage] = React.useState('english');
 
   const data = allData[language];
+ 
   return (
     <div className="App">
+      <div>
       <select onChange={e => setLanguage(e.target.value)}>
         <option value="english">English</option>
         <option value="bulgarian">Bulgarian</option>
+    
       </select>
+      <button id="btn" onClick={generatePDF}>Download PDF</button>
+   </div>
 
-      <div className="page">
+      <div className="page" id="page">
+ 
         <div className="column left">
           <Image img={data.img} />
           <Details data={data.contact} />
@@ -50,6 +92,7 @@ function App() {
           <Education data={data.education} />
         </div>
       </div>
+
     </div>
   );
 }
